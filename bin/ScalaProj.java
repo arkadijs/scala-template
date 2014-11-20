4a3129e24f28dd59dfb9a385b6528696e08d65e3
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2014-11-20 19:16:21 +0200

    Replace perl scalaproj with shell + pure-Java

---
+++ bin/ScalaProj.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class ScalaProj {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.err.println("Usage: ScalaProj <template-dir> <new-project-dir>");
+            System.exit(1);
+
+        }
+        String template = args[0];
+        String project  = args[1];
+        String pkg      = project.replaceAll("[_+-]", "").toLowerCase();
+
+        FileSystem fs = FileSystems.getDefault();
+
+        copy(fs.getPath(template), fs.getPath(project));
+
+        List<String> files = Arrays.asList(new String[] {
+            "build.sbt",
+            "pom.xml",
+            "src/main/scala/package/Main.scala",
+            "src/test/scala/package/test/Test.scala",
+            "src/main/resources/log4j.properties",
+            "src/test/resources/log4j.properties"
+        });
+        String cs = "UTF-8";
+        for (String file : files) {
+            String content = new String(Files.readAllBytes(fs.getPath(template, file)), cs);
+            content = content.replaceAll("\\$project", project).replaceAll("\\$package", pkg);
+            Files.write(fs.getPath(project, file), content.getBytes(cs), StandardOpenOption.TRUNCATE_EXISTING);
+        }
+
+        List<String> dirs = Arrays.asList(new String[]{
+                "src/main/scala/package",
+                "src/test/scala/package"
+        });
+        for (String dir : dirs) {
+            Path path = fs.getPath(project, dir);
+            Files.move(path, path.resolveSibling(pkg));
+        }
+    }
+
+    private static void copy(final Path source, final Path target) throws IOException {
+        Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                if (dir.toFile().getName().startsWith(".")) return FileVisitResult.SKIP_SUBTREE;
+                Files.createDirectories(target.resolve(source.relativize(dir)));
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (file.toFile().getName().toLowerCase().startsWith("scalaproj")) return FileVisitResult.SKIP_SUBTREE;
+                Files.copy(file, target.resolve(source.relativize(file)));
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+}
